cc44d7797f3937a0c045d194c82afae7f5797039
Anjan <anjanc@syncroutesoftware.com>
2018-05-22 12:19:57 +0530

    add some commment and add README file

---
+++ README.md
@@ -0,0 +1,7 @@
+This is my URL shorting APP based on Rails
+1.for data base i am using postgres.
+2.for test cases i am using Respec.
+3. My app is deployed on Heroku.
+4. I have created 2 model one is for store the request url and shorted url  another one is the sttes of each url and there is a relation each sates belonges to (belonges_to)one URL and URL have many states.(hasmany)
+5. I have used rails view and root page is new methos that will create data and store to db and redirect to index page and in inex page displaying each data nad from each you can see all the sates by clicking CHECK botton.
+6.I have written model and controller all test cases.
--- app/models/stored_url.rb
+++ app/models/stored_url.rb
@@ -4,12 +4,12 @@ class StoredUrl < ActiveRecord::Base
   has_many :url_infos
   validates :original_url, presence: true, on: :create
   validates_format_of :original_url, with: /\A(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-]*)*\/?\Z/i
-  #validates_format_of :original_url,
-  #with: /\A(?:(?:http|https):\/\/)?([-a-zA-Z0-9.]{2,256}\.[a-z]{2,4})\b(?:\/[a-aZ-Z0-9@,!:%_\+.~#?&\/\/=]*)?\z/
+  
   before_create :crerate_optimize_url
   before_create :cleaning_url
 
   def crerate_optimize_url
+    #creating the random 7 char short url
   	#new_url = Random.new.bytes(6) + rand(100..10000).to_s
   	new_url = ([*('a'..'z'),*('0'..'9')]).sample(SHORT_URL_LENGTH).join
   	old_url = StoredUrl.where(short_url: new_url).last
@@ -30,6 +30,7 @@ class StoredUrl < ActiveRecord::Base
 
   def cleaning_url
   	self.original_url.strip
+    #cleaning and filtering the real url
   	#self.purify_url = self.original_url.downcase.gsub(/(https?:\/\/)|(www\.)/, "")
   	self.purify_url = "http://#{self.original_url.downcase.gsub(/(https?:\/\/)|(www\.)/, "")}"
   end
